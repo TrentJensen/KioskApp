db5a81100afc24bfeac5223ad315fada2fb50142
TrentJensen <d.trentjensen@gmail.com>
2018-04-27 17:13:30 -0600

    Add orders for vendor

--- KioskApp/Controllers/OrderController.cs
+++ KioskApp/Controllers/OrderController.cs
@@ -14,14 +14,16 @@ namespace KioskApp.Controllers
         private readonly IOrderRepository _orderRepository;
         private readonly ICustomerRepository _customerRepository;
         private readonly IProductRepository _productRepository;
+        private readonly IVendorRepository _vendorRepository;
         private readonly UserManager<ApplicationUser> _userManager;
 
         public OrderController(IOrderRepository orderRepository, ICustomerRepository customerRepository,
-            IProductRepository productRepository, UserManager<ApplicationUser> userManager)
+            IProductRepository productRepository, IVendorRepository vendorRepository, UserManager<ApplicationUser> userManager)
         {
             _orderRepository = orderRepository;
             _customerRepository = customerRepository;
             _productRepository = productRepository;
+            _vendorRepository = vendorRepository;
             _userManager = userManager;
         }
         public IActionResult Index()
@@ -29,11 +31,29 @@ namespace KioskApp.Controllers
             //Get the Guid of the current user from the UserManager
             var userGuid = _userManager.GetUserId(HttpContext.User);
             var customer = _customerRepository.GetCustomerByGuid(userGuid);
+            var vendor = _vendorRepository.Vendors.FirstOrDefault(v => v.LoginId == userGuid);
+
             OrderViewModel orderViewModel = new OrderViewModel();
 
             if (customer != null)
             {
                 IEnumerable<Order> orders = _orderRepository.GetOrdersByCustomerId(customer.Id);
+                foreach (Order order in orders)
+                {
+                    IEnumerable<OrderList> orderLists = _orderRepository.GetOrderListsByOrderId(order.Id);
+                    foreach (OrderList list in orderLists)
+                    {
+                        Product product = _productRepository.GetProductbyId(list.ProductId);
+                        list.Product.Name = product.Name;
+                    }
+                    order.OrderLines = orderLists.ToList();
+                }
+                orderViewModel.Orders = orders;
+            }
+
+            if (vendor != null)
+            {
+                IEnumerable<Order> orders = _orderRepository.GetOrdersByVendorId(vendor.Id);
                 foreach (Order order in orders)
                 {
                     IEnumerable<OrderList> orderLists = _orderRepository.GetOrderListsByOrderId(order.Id);
